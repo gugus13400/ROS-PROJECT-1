570b1d1115f08075cf5c3f12b9e602706b744d6f
Auguste <augustepugnet1@gmail.com>
2020-02-14 17:57:13 +0000

    push

--- dijkstra_planner.py
+++ dijkstra_planner.py
@@ -53,13 +53,16 @@ class DIJKSTRAPlanner(CellBasedForwardSearch):
 
             while (self.dijkstraQueue.empty() == False):
                 store = self.dijkstraQueue.get()
-                print('The store [0] element is : ' + str(store[0]) + ' and the store[1] element is ' + str(store[1]) )
-                print('The nextCell pathcost element is : ' + str(nextCell.pathCost) + ' and the nextcell element is ' + str(nextCell))
+                # print('The store [0] element is : ' + str(store[0]) + ' and the store[1] element is ' + str(store[1]) )
+                # print('The nextCell pathcost element is : ' + str(nextCell.pathCost) + ' and the nextcell element is ' + str(nextCell))
                 if store[1].coords[0] != nextCell.coords[0]:
                    self.temporaryQueue.put((store[0], store[1]))
-                   print(str(store[1].coords[0]))
                 elif store[1].coords[0] == nextCell.coords[0] and store[1].coords[1] == nextCell.coords[1]:
                     self.temporaryQueue.put((nextCell.pathCost, nextCell))
+                    print(str(store[1].coords[0]))
+                    print(str(store[1].coords[1]))
+                    print(str(nextCell[1].coords[0]))
+                    print(str(nextCell[1].coords[1]))
 
             # while (self.temporaryQueue.empty() == False):
             #     tempo = self.temporaryQueue.get()
