69a4f6c356dc3418d8dfe178d18c1081616e3708
gugus13400 <augustepugnet1@gmail.com>
2020-02-24 11:18:40 +0000

    caca

--- archive/move2goal_controller.py
+++ archive/move2goal_controller.py
@@ -7,7 +7,6 @@ from comp0037_planner_controller.planned_path import PlannedPath
 from comp0037_planner_controller.controller_base import ControllerBase
 import math
 import angles
-import time
 
 # This sample controller works a fairly simple way. It figures out
 # where the goal is. It first turns the robot until it's roughly in
@@ -15,7 +14,6 @@ import time
 # angular error and trims it as it goes.
 
 class Move2GoalController(ControllerBase):
-
 
     def __init__(self, occupancyGrid):
         ControllerBase.__init__(self, occupancyGrid)
@@ -25,11 +23,7 @@ class Move2GoalController(ControllerBase):
         self.angleErrorGain = rospy.get_param('angle_error_gain', 4)
 
         self.driveAngleErrorTolerance = math.radians(rospy.get_param('angle_error_tolerance', 1))
-
-	self.totalTime = 0
-	self.timeForEachLoop = 0
-
-	
+    
     def get_distance(self, goal_x, goal_y):
         distance = sqrt(pow((goal_x - self.pose.x), 2) + pow((goal_y - self.pose.y), 2))
         return distance
@@ -51,16 +45,10 @@ class Move2GoalController(ControllerBase):
         angleError = self.shortestAngularDistance(self.pose.theta, atan2(dY, dX))
        
         while (distanceError >= self.distanceErrorTolerance) & (not rospy.is_shutdown()):
-	    self.loopStartTime = time.time()
-
-            #print("Current Pose: x: {}, y:{} , theta: {}\nGoal: x: {}, y: {}".format(self.pose.x, self.pose.y,
+            #print("Current Pose: x: {}, y:{} , theta: {}\nGoal: x: {}, y: {}\n".format(self.pose.x, self.pose.y,
             #                                                                           self.pose.theta, waypoint[0],
             #                                                                           waypoint[1]))
-            
-	    print("Distance Error: {}\nAngular Error: {}".format(distanceError, angleError))
-
-            print("Total Distance Travelled: {}\nTotal Angle Turned: {}".format(self.PathLength, self.TotalTheta))
-            
+            print("Distance Error: {}\nAngular Error: {}".format(distanceError, angleError))
 
             # Proportional Controller
             # linear velocity in the x-axis: only switch on when the angular error is sufficiently small
@@ -78,12 +66,6 @@ class Move2GoalController(ControllerBase):
             #print("Linear Velocity: {}\nAngular Velocity: {}\n\n".format(vel_msg.linear.x, math.degrees(vel_msg.angular.z)))
             # Publishing our vel_msg
             self.velocityPublisher.publish(vel_msg)
-
-	    self.messagePublishTime = time.time()
-	    self.timeForEachLoop = self.messagePublishTime - self.loopStartTime
-	    self.totalTime += self.timeForEachLoop		
-	    print("Total Time Elapsed: {}\n".format(self.totalTime))
-				
             if (self.plannerDrawer is not None):
                 self.plannerDrawer.flushAndUpdateWindow()
                 
@@ -92,13 +74,11 @@ class Move2GoalController(ControllerBase):
             distanceError = sqrt(pow((waypoint[0] - self.pose.x), 2) + pow((waypoint[1] - self.pose.y), 2))
             angleError = self.shortestAngularDistance(self.pose.theta,
                                                       atan2(waypoint[1] - self.pose.y, waypoint[0] - self.pose.x))
-            
 
         # Make sure the robot is stopped once we reach the destination.
         vel_msg.linear.x = 0
         vel_msg.angular.z = 0
         self.velocityPublisher.publish(vel_msg)
-
 
     def rotateToGoalOrientation(self, goalOrientation):
         vel_msg = Twist()
