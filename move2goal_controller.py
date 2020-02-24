d9a489a56e401941e0e89ebddbc2a0c331f542f8
Auguste <augustepugnet1@gmail.com>
2020-02-24 12:27:03 +0000

    push

--- move2goal_controller.py
+++ move2goal_controller.py
@@ -51,6 +51,9 @@ class Move2GoalController(ControllerBase):
         distanceError = sqrt(dX * dX + dY * dY)
         angleError = self.shortestAngularDistance(self.pose.theta, atan2(dY, dX))
 
+        self.totalTime = 0
+        self.timeForEachLoop = 0
+        self.loopStartTime = 0
         self.loopStartTime = time.time()
         while (distanceError >= self.distanceErrorTolerance) & (not rospy.is_shutdown()):
 
